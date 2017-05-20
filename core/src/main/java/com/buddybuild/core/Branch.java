2dea96bec1d19cd767e791262d3b601b2ff05090
Jason Atwood <jason.atwood@stablekernel.com>
2017-05-20 18:11:33 -0400

    Add created, started, and finished times to Build model

--- core/src/main/java/com/buddybuild/core/Branch.java
+++ core/src/main/java/com/buddybuild/core/Branch.java
@@ -1,5 +1,8 @@
 package com.buddybuild.core;
 
+import org.threeten.bp.ZonedDateTime;
+
+import java.util.Comparator;
 import java.util.List;
 
 import lombok.Getter;
@@ -19,5 +22,65 @@ public class Branch {
 
     public Branch(String name) {
         this.name = name;
+    }
+
+    /**
+     * TODO
+     *
+     * @return
+     */
+    public ZonedDateTime getMostRecentBuildEvent() {
+        if (builds == null || builds.isEmpty()) {
+            return null;
+        }
+
+        ZonedDateTime mostRecent = null;
+        for (Build build : builds) {
+            if (mostRecent == null) {
+                mostRecent = build.getMostRecentBuildEvent();
+                continue;
+            }
+
+            if (mostRecent.isBefore(build.getMostRecentBuildEvent())) {
+                mostRecent = build.getMostRecentBuildEvent();
+            }
+        }
+
+        return mostRecent;
+    }
+
+    /**
+     * A Comparator that sorts Branches, with "master" first, then remaining by most recent event.
+     * If most recent event is null then puts last.
+     */
+    public static class SortByMostRecentEvent implements Comparator<Branch> {
+
+        @Override
+        public int compare(Branch branch1, Branch branch2) {
+            if (branch1.name.equals("master")) {
+                return -1;
+            }
+
+            if (branch2.name.equals("master")) {
+                return 1;
+            }
+
+            ZonedDateTime branch1MostRecentEvent = branch1.getMostRecentBuildEvent();
+            ZonedDateTime branch2MostRecentEvent = branch2.getMostRecentBuildEvent();
+
+            // if any are null
+            if (branch1MostRecentEvent == null && branch2MostRecentEvent == null) {
+                return 1;
+            } else if (branch1MostRecentEvent == null) {
+                return 1;
+            } else if (branch2MostRecentEvent == null)
+                return -1;
+
+            if (branch1MostRecentEvent.isAfter(branch2MostRecentEvent)) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
     }
 }
--- core/src/main/java/com/buddybuild/core/Build.java
+++ core/src/main/java/com/buddybuild/core/Build.java
@@ -1,11 +1,14 @@
 package com.buddybuild.core;
 
+import org.threeten.bp.ZonedDateTime;
+
 import lombok.Getter;
 
 /**
  * A model of a build
  */
 public class Build {
+
     @Getter
     private String id;
 
@@ -24,13 +27,39 @@ public class Build {
     @Getter
     private String commitMessage;
 
-    private Build(String id, Boolean buildStatus, String branch, Integer buildNumber, String author, String commitMessage) {
+    @Getter
+    private ZonedDateTime createTime;
+
+    @Getter
+    private ZonedDateTime startTime;
+
+    @Getter
+    private ZonedDateTime finishTime;
+
+    /**
+     * @return most recent build event: finished, started, then created... or null
+     */
+    public ZonedDateTime getMostRecentBuildEvent() {
+        if (finishTime != null) {
+            return finishTime;
+        }
+        if (startTime != null) {
+            return startTime;
+        }
+        return createTime;
+    }
+
+    private Build(String id, Boolean buildStatus, String branch, Integer buildNumber, String author, String
+            commitMessage, ZonedDateTime createTime, ZonedDateTime startTime, ZonedDateTime finishTime) {
         this.id = id;
         this.buildStatus = buildStatus;
         this.branch = branch;
         this.buildNumber = buildNumber;
         this.author = author;
         this.commitMessage = commitMessage;
+        this.createTime = createTime;
+        this.startTime = startTime;
+        this.finishTime = finishTime;
     }
 
     public static class Builder {
@@ -40,6 +69,9 @@ public class Build {
         private Integer buildNumber;
         private String author;
         private String commitMessage;
+        private ZonedDateTime createTime;
+        private ZonedDateTime startTime;
+        private ZonedDateTime finishTime;
 
         public Builder id(String id) {
             this.id = id;
@@ -71,9 +103,24 @@ public class Build {
             return this;
         }
 
-        public Build build() {
-            return new Build(id, buildStatus, branch, buildNumber, author, commitMessage);
+        public Builder createTime(ZonedDateTime createTime) {
+            this.createTime = createTime;
+            return this;
         }
 
+        public Builder startTime(ZonedDateTime startTime) {
+            this.startTime = startTime;
+            return this;
+        }
+
+        public Builder finishTime(ZonedDateTime finishTime) {
+            this.finishTime = finishTime;
+            return this;
+        }
+
+        public Build build() {
+            return new Build(id, buildStatus, branch, buildNumber, author, commitMessage, createTime, startTime,
+                    finishTime);
+        }
     }
 }
